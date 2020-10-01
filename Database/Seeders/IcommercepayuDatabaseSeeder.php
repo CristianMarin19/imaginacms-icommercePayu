f1ce72ee0e287f12108f40f89a70e9baedd9ef35
JCEC007 <jcec007@gmail.com>
2020-10-01 11:51:00 -0500

    +add: validate seed if the methods its already seeded

--- Database/Seeders/IcommercepayuDatabaseSeeder.php
+++ Database/Seeders/IcommercepayuDatabaseSeeder.php
@@ -16,7 +16,10 @@ class IcommercepayuDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-
+  
+      $paymentMethod = PaymentMethod::findByAttributes(["name" => config('asgard.icommercepayu.config.paymentName')]);
+      
+      if(!isset($paymentMethod->id)){
         $options['init'] = "Modules\Icommercepayu\Http\Controllers\Api\IcommercePayuApiController";
         $options['mainimage'] = null;
         $options['merchantId'] = "508029";
@@ -25,35 +28,37 @@ class IcommercepayuDatabaseSeeder extends Seeder
         $options['accountId'] = "512321";
         $options['mode'] = "sandbox";
         $options['test'] = 1;
-
+  
         $titleTrans = 'icommercepayu::icommercepayus.single';
         $descriptionTrans = 'icommercepayu::icommercepayus.description';
-
+  
         foreach (['en', 'es'] as $locale) {
-
-            if($locale=='en'){
-                $params = array(
-                    'title' => trans($titleTrans),
-                    'description' => trans($descriptionTrans),
-                    'name' => config('asgard.icommercepayu.config.paymentName'),
-                    'status' => 0,
-                    'options' => $options
-                );
-
-                $paymentMethod = PaymentMethod::create($params);
-
-            }else{
-
-                $title = trans($titleTrans,[],$locale);
-                $description = trans($descriptionTrans,[],$locale);
-
-                $paymentMethod->translateOrNew($locale)->title = $title;
-                $paymentMethod->translateOrNew($locale)->description = $description;
-
-                $paymentMethod->save();
-            }
-
+    
+          if($locale=='en'){
+            $params = array(
+              'title' => trans($titleTrans),
+              'description' => trans($descriptionTrans),
+              'name' => config('asgard.icommercepayu.config.paymentName'),
+              'status' => 0,
+              'options' => $options
+            );
+      
+            $paymentMethod = PaymentMethod::create($params);
+      
+          }else{
+      
+            $title = trans($titleTrans,[],$locale);
+            $description = trans($descriptionTrans,[],$locale);
+      
+            $paymentMethod->translateOrNew($locale)->title = $title;
+            $paymentMethod->translateOrNew($locale)->description = $description;
+      
+            $paymentMethod->save();
+          }
+    
         }// Foreach
-
+  
+      }
+      
     }
 }
