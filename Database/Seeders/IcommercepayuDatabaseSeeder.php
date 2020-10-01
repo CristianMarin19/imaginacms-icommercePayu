a881042670a8126a79d7f98826c87675a172b48d
JCEC007 <jcec007@gmail.com>
2020-10-01 12:00:33 -0500

    +fix: change findByAttributes by firstWhere

--- Database/Seeders/IcommercepayuDatabaseSeeder.php
+++ Database/Seeders/IcommercepayuDatabaseSeeder.php
@@ -17,7 +17,7 @@ class IcommercepayuDatabaseSeeder extends Seeder
     {
         Model::unguard();
   
-      $paymentMethod = PaymentMethod::findByAttributes(["name" => config('asgard.icommercepayu.config.paymentName')]);
+      $paymentMethod = PaymentMethod::firstWhere("name", config('asgard.icommercepayu.config.paymentName'));
       
       if(!isset($paymentMethod->id)){
         $options['init'] = "Modules\Icommercepayu\Http\Controllers\Api\IcommercePayuApiController";
